6e8a79d6d0c89edc1cb193231fa33e9d506d475c
jdelarubia <jdelarubia@gmail.com>
2022-01-13 22:22:41 +0100

    minimal dist

---
+++ dist/js/bundle.js
@@ -0,0 +1,7 @@
+(function () {
+	'use strict';
+
+	console.log("testing component....");
+
+})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwic291cmNlcyI6WyIuLi8uLi9zcmMvanMvbWFpbi5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcclxuY29uc29sZS5sb2coXCJ0ZXN0aW5nIGNvbXBvbmVudC4uLi5cIik7XHJcbiJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Q0FDQSxPQUFPLENBQUMsR0FBRyxDQUFDLHVCQUF1QixDQUFDOzs7Ozs7In0=
