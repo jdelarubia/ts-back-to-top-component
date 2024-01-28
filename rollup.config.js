14a1b2f4d9330d452a3facb4acded69092e26a84
jdelarubia <jdelarubia@gmail.com>
2024-01-28 22:39:24 +0100

    reformat rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,18 @@
 /**
  * rollup.config.js
+ * Rollup configuration file.
  */
 
- 
- export default {
-     input: "./src/js/main.js",
-     watch: {
-         include: "./src/js/**",
-     },
-     output: {
-         file: "./dist/js/bundle.js",
-         format: "iife",
-         sourcemap: "inline"
-     },
- };
- 
+export default [
+  {
+    input: './src/js/main.js',
+    watch: {
+      include: './src/js/main.js',
+    },
+    output: {
+      file: './dist/js/bundle.js',
+      format: 'iife',
+      sourcemap: 'inline',
+    },
+  },
+]
