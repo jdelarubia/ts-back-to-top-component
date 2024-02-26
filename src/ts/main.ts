244d13706db89f09a4735e26838f93ccc8106cb9
jdelarubia <jdelarubia@gmail.com>
2024-02-26 22:24:16 +0100

    refactor main to reflect changes in components

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,20 @@
 import { BackToTopBtn } from './back-to-top-btn'
 import { FancyTextBlock } from './fancy-text-block'
+import { InstagramBtn } from './instagram-btn'
 
-new BackToTopBtn()
-new FancyTextBlock()
+window.customElements.define('back-to-top-btn', BackToTopBtn)
+window.customElements.define('fancy-text-block', FancyTextBlock)
+window.customElements.define('instagram-btn', InstagramBtn)
+
+// Example of creating fancy text programmatically
+// new FancyTextBlock()
+
+// Example of creating instagram button programmatically
+// new InstagramBtn('https://www.instagram.com')
+// const body = document.querySelector('body')
+// body?.appendChild(backToTopBtn)
+
+// Example of creating back-to-top button programmatically
+// const backToTopBtn = new BackToTopBtn('top')
+// const body = document.querySelector('body')
+// body?.appendChild(backToTopBtn)
